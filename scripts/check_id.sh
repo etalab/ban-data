bd1b8a93b62eca88681fb5e4260fd9c338c9a48d
Christian Quest <cquest@openstreetmap.fr>
2015-03-20 14:42:31 +0100

    script de contrôle du champ ID

---
+++ scripts/check_id.sh
@@ -0,0 +1,9 @@
+echo "\n-- nombre d'ID vides, nuls ou ne commençant par par ADR\n"
+psql -c "select left(code_insee,2) as dept, count(*) as nb from ban_temp where id is null or id='' or id not like 'ADR%' group by 1;" -P pager
+
+echo "\n-- comparaison nombre total d'adresses et ID distincts par département\n"
+psql -c "select left(code_insee,2) as dept, count(*) as total, count(distinct(id)) as id_distincts, count(*)-count(distinct(id)) as doublons from ban_temp group by 1 order by 1;" -P pager
+
+echo "\n-- nombre de doublons par département\n"
+psql -c "select left(code_insee,2) as dept, sum(nb) as nb_id_doublons from (select id, code_insee, count(*) as nb from ban_temp group by 1,2) as ids where nb>1 group by 1 order by 1;" -P pager
+
