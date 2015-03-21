7ecd36388735f6b9954898f61a3d476dff505df7
Christian Quest <cquest@openstreetmap.fr>
2015-03-21 11:07:11 +0100

    plus de tests sur les noms de voie

--- scripts/check_nom_voie.sh
+++ scripts/check_nom_voie.sh
@@ -8,4 +8,21 @@ echo "\n-- nombre de nom_voie avec '/' et répétitions (regroupés par départe
 psql -P pager -c "select left(code_insee,2) as dept, count(*) as nb_total, sum(repete) as nb_repete, min(nom_voie) as exemple from (select code_insee, nom_voie, array_length(regexp_matches(nom_voie,'^(.*)/\1$'),1) as repete from ban_temp where nom_voie like '%/%') as r group by 1 order by 1;;"
 
 echo "\n-- nombre de nom_voie différents avec id_fantoir identique (regroupés par département)\n"
+psql -P pager -c "select left(code_insee,2) as dept, count(*) as nb, min(noms) as exemple from (select code_insee, id_fantoir, count(*) as nb_noms, sum(nb) as nb_adresses, left(string_agg(nom,' + '),100) as noms from (select code_insee, id_fantoir, format('"%s,%s"',nom_voie,nom_ld) as nom, count(*) as nb from ban_temp where id_fantoir !='' group by 1,2,3) as f group by 1,2) as f2 where nb_noms>1 group by 1 order by 1;"
+
+echo "\n-- exemples de nom_voie différents avec id_fantoir identique\n"
+psql -P pager -c "select code_insee, id_fantoir, count(*) as nb_noms, sum(nb) as nb_adresses, left(string_agg(nom,' + '),100) as noms from (select code_insee, id_fantoir, format('"%s,%s"',nom_voie,nom_ld) as nom, count(*) as nb from ban_temp where id_fantoir !='' group by 1,2,3) as f group by 1,2 order by 3 desc limit 50;"
+
+echo "\n-- vérification erreurs courantes d'accentuation\n"
+psql -P pager -c "select nom_voie, count(*) as nb from ban_temp where nom_voie ~ ' clémenceau' group by 1 order by 2 desc;"
+
+echo "\n-- vérification chiffres romains en minuscule\n"
+psql -P pager -c "select nom_voie, count(*) as nb from ban_temp where nom_voie ~ ' [ivx]*( |$)' and nom_voie !~ 'vi?[vx]' group by 1 order by 2 desc;"
+
+echo "\n-- vérification de présence d'abbréviations résiduelles\n"
+psql -P pager -c "select nom_voie, count(*) as nb from ban_temp where nom_voie ~ '(^| )(vx|ht|hte|chem|bd|pl|av|prom)( |$)' group by 1 order by 2 desc;"
+
+echo "\n-- vérification de présence d'abbréviations doublées\n"
+psql -P pager -c "select nom_voie, count(*) from ban_temp where nom_voie ~ '(^| )(chemin .*chem|grand.*gde)( |$)' group by 1 order by 2 desc;"
+
 
