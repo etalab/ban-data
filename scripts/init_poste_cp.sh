b5e774499f2dc6753612b67a1e4a54037bbc103c
Christian Quest <cquest@openstreetmap.fr>
2015-03-22 12:41:07 +0100

    correction \copy sur ligne unique

--- scripts/init_poste_cp.sh
+++ scripts/init_poste_cp.sh
@@ -5,10 +5,9 @@ cd ../data/poste/
 wget -nc https://www.data.gouv.fr/s/resources/base-officielle-des-codes-postaux/community/20150308-152148/code_postaux_v201410_corr.csv
 
 # import dans postgres
+psql -c "drop table if exists poste_cp; create table poste_cp (insee text, commune text, cp text, libelle text);"
+psql -c "\copy poste_cp from code_postaux_v201410_corr.csv with (format csv, header true, encoding 'iso8859-1', delimiter ';');"
 psql -c "
-drop table if exists poste_cp;
-create table poste_cp (insee text, commune text, cp text, libelle text);
-\copy poste_cp from code_postaux_v201410_corr.csv with (format csv, header true, encoding 'iso8859-1', delimiter ';');
 create index poste_cp_insee on poste_cp using spgist (insee);
 create index poste_cp_cp on poste_cp using spgist (cp);
 "
