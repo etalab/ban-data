f9f4de2546dba98e7f63c3ef9319bf525427738b
Christian Quest <cquest@openstreetmap.fr>
2015-03-22 12:38:56 +0100

    import COG 2014 de l'INSEE

--- scripts/init_cog.sh
+++ scripts/init_cog.sh
@@ -8,12 +8,20 @@ wget -nc http://www.collectivites-locales.gouv.fr/files/files/epcicom2015.csv
 csvcut -e iso-8859-1 -c 10,12,13 epcicom2015.csv -d ";" > cog_temp.csv
 
 # import dans postgres
-psql -c "
-drop table if exists cog_temp;
+psql -c "drop table if exists cog_temp;create table cog_temp (insee text, nom text, population numeric);"
+psql -c "\copy cog_temp from cog_temp.csv with (format csv, header true);"
+psql -c "create index cog_temp_insee on cog_temp using spgist (insee);"
 
-create table cog_temp (insee text, nom text, population numeric);
-\copy cog_temp from cog_temp.csv with (format csv, header true);
+# COG 2014 de l'INSEE
+wget -nc http://www.insee.fr/fr/methodes/nomenclatures/cog/telechargement/2014/txt/comsimp2014.zip
+unzip -o comsimp2014.zip
+# conversion en CSV UTF-8
+cat comsimp2014.txt | iconv -f iso88591 -t utf8 | tr '\t' ',' > comsimp2014.csv
 
-create index cog_temp_insee on cog_temp using spgist (insee);
-"
+psql -c "drop table if exists insee_cog_2014;create table insee_cog_2014 (CDC text,CHEFLIEU text,REG text,DEP text,COM text,AR text,CT text,TNCC text,ARTMAJ text,NCC text,ARTMIN text,NCCENR text);"
+psql -c "\copy insee_cog_2014 from comsimp2014.csv with (format csv, header true);"
+psql -c "alter table insee_cog_2014 add column insee text; update insee_cog_2014 set insee=dep||com;"
+psql -c "create index insee_cog_2014_insee on insee_cog_2014 using spgist(insee);"
+psql -c "update insee_cog_2014 set nccenr='Fœil' where insee='22059';" # problème d'encodage dans le fichier source ?
+
 
