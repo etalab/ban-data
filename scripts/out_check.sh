d4eed83e93335552b18a729035e8bf7ecd0057ad
Christian Quest <cquest@openstreetmap.fr>
2015-03-29 23:41:22 +0200

    bilan anomalies livraison du 2015-03-24

--- scripts/out_check.sh
+++ scripts/out_check.sh
@@ -2,7 +2,7 @@
 
 # import dans postgres
 psql -c "drop table if exists errors; create table errors (insee text, id text, col text, val text, info text);"
-psql -c "\copy errors from erreurs.csv with (format csv, header true);"
+psql -c "\copy errors from erreurs.csv with (format csv, header false);"
 
 echo "\n-- nombre d'adresse avec anomalie (groupé par département)\n"
 psql -c "\copy (select e.dept, nb_err, nb_adr, round(100*nb_err/nb_adr,1) as pct_err from (select left(insee,2) as dept, count(distinct(id)) as nb_err from errors group by 1) as e join (select left(code_insee,2) as dept, count(*) as nb_adr from ban_temp group by 1) as a on (e.dept=a.dept) order by 1) to out_check_par_dept.csv with (format csv, header true);"
