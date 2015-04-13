7265c51d398a71b2b996d530e883d7562ae47f6e
Christian Quest <cquest@openstreetmap.fr>
2015-04-13 18:34:44 +0200

    export via out_pg2json.sh

--- scripts/convert_ban2json.sh
+++ scripts/convert_ban2json.sh
@@ -34,36 +34,6 @@ psql -q < $TEMPDIR/clean_$1.sql
 rm $TEMPDIR/clean_$1.sql
 
 echo "`date +%H:%M:%S` Export JSON dept $1"
-# export postgres au format json pour addok
-psql --no-align --tuples-only -qc "
-select format('{\"id\":\"%s_%s\",\"type\":\"%s\",\"name\":\"%s\",\"postcode\":\"%s\",\"lon\":%s,\"lat\": %s,\"city\":\"%s\",\"departement\":\"%s\",\"region\":\"%s\",\"importance\":%s,\"housenumbers\":{%s}}',code_insee, fantoir, type, nom_voie, code_post, lat, lon, nom_commune, nom_dep, nom_reg, importance, housenumbers)
-from
-(select code_insee,
-
-(case when id_ld is not null AND coalesce(id_voie,'')!=coalesce(id_ld,'') then coalesce(id_voie,'')||id_ld
-when coalesce(id_voie,'')!='' and nom_ld='' then coalesce(id_voie,'')
-when id_ld is not null then id_ld
-when coalesce(id_voie,'')!='' and nom_ld!='' then coalesce(id_voie,'')
-else
-'XXXX' end) || '_' || left(md5(format('n=%s,l=%s,a=%s,p=%s',nom_voie,nom_ld,alias,code_post)),6)
- as fantoir,
-
-case when (coalesce(nom_voie,'') !='' and coalesce(nom_ld,'') !='' and replace(upper(unaccent(coalesce(nom_voie,''))),'-',' ')!=replace(upper(unaccent(coalesce(nom_ld,''))),'-',' ')) then (coalesce(nom_voie,'')||', '||coalesce(nom_ld,'')) when (coalesce(nom_voie,'')='') then nom_ld else nom_voie end as nom_voie,
-code_post,
-round(avg(lat::numeric),6) as lat,
-round(avg(lon::numeric),6) as lon,
-regexp_replace(nom_commune,' [0-9].*','') as nom_commune,
-nom_dep,
-nom_reg,
-case when id_fantoir > '9999' then 'place' else 'street' end as type,
-round(log((CASE WHEN (code_post LIKE '75%' OR g.statut LIKE 'Capital%') THEN 6 WHEN (code_post LIKE '690%' OR code_post LIKE '130%' OR g.statut = 'Préfecture de régi') THEN 5 WHEN g.statut='Préfecture' THEN 4 WHEN g.statut LIKE 'Sous-pr%' THEN 3 WHEN g.statut='Chef-lieu canton' THEN 2 ELSE 1 END)+log(g.population+1)/3)::numeric*log(1+log(count(b.*)+1)+log(CASE WHEN nom_voie like 'Boulevard%' THEN 4 WHEN nom_voie LIKE 'Place%' THEN 4 WHEN nom_voie LIKE 'Espl%' THEN 4 WHEN nom_voie LIKE 'Av%' THEN 3 WHEN nom_voie LIKE 'Rue %' THEN 2 ELSE 1 END))::numeric,4)::text as importance,
-string_agg(format('\"%s\":{\"lat\":%s,\"lon\":%s,\"id\":\"%s\"}',trim(numero||rep),round(lon::numeric,6)::text,round(lat::numeric,6)::text,id),',' order by numero||rep,id) as housenumbers
-from ban_$1 b
-join osm_communes g on (g.insee=code_insee)
-join cog_dep d on (d.dep=left(code_insee,2) or d.dep=left(code_insee,3))
-join cog_reg r on (r.reg=d.reg)
-group by 1,2,3,4,7,8,9,10,g.statut,g.population,nom_voie
-order by 1,2,3) as d;
-" > ../out/ban-odbl-$1.json
+sh out_pg2json.sh $1 > ../out/ban-odbl-$1.json
 echo "`date +%H:%M:%S` Terminé dept $1"
 
