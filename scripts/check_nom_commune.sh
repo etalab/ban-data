3bfd03c44f348180b55130d662959329230aff6a
Christian Quest <cquest@openstreetmap.fr>
2015-04-30 19:18:37 +0200

    passage au COG2015

--- scripts/check_nom_commune.sh
+++ scripts/check_nom_commune.sh
@@ -10,8 +10,8 @@ sql2csv --db "$DB" -H --query "select code_insee,id,'nom_commune','','nom_commun
 echo "\n-- nom_commune inconnu\n"
 sql2csv --db "$DB" -H --query "select code_insee,id,'code_post',code_post,'code_post inconnu' from ban_temp b left join poste_cp p on (p.cp=b.code_post) where p.cp is null and b.code_post !=''" >> erreurs.csv
 
-echo "\n-- nom_commune ne correspond pas au COG 2014\n"
-sql2csv --db "$DB" -H --query "select b.code_insee, b.id, 'nom_commune', b.nom_commune, 'nom_commune ne correspond pas au COG 2014: ' || trim(replace(replace(c.artmin||' '||c.nccenr,'(',''),')','')) from ban_temp b left join insee_cog_2014 c on (c.insee=b.code_insee) where trim(replace(replace(c.artmin||c.nccenr,'(',''),')','')) != replace(b.nom_commune,' ','') and NOT b.nom_commune ~ 'rondissement$'" >> erreurs.csv
+echo "\n-- nom_commune ne correspond pas au COG 2015\n"
+sql2csv --db "$DB" -H --query "select b.code_insee, b.id, 'nom_commune', b.nom_commune, 'nom_commune ne correspond pas au COG 2015: ' || trim(replace(replace(c.artmin||' '||c.nccenr,'(',''),')','')) from ban_temp b left join insee_cog_2015 c on (c.insee=b.code_insee) where trim(replace(replace(c.artmin||c.nccenr,'(',''),')','')) != replace(b.nom_commune,' ','') and NOT b.nom_commune ~ 'rondissement$'" >> erreurs.csv
 
 echo "\n-- nom_commune = nom_ld"
 sql2csv --db "$DB" -H --query "select b.code_insee, b.id, 'nom_commune', b.nom_commune, 'nom_commune et nom_ld identiques: ' || nom_ld from ban_temp b where nom_ld !='' and replace(replace(unaccent(lower(nom_commune)),'-',' '),chr(39),' ')=replace(replace(lower(nom_ld),'-',' '),chr(39),' ')" >> erreurs.csv
