ef669189bf24a5b8ed7e6f8b07e13b93f98ed00f
Christian Quest <cquest@openstreetmap.fr>
2015-05-19 18:31:51 +0200

    id manquait dans le test CP absent base officielle

--- scripts/check_code_post.sh
+++ scripts/check_code_post.sh
@@ -11,5 +11,5 @@ echo "\n-- code_post inconnu\n"
 sql2csv --db "$DB" -H --query "select code_insee,id,'code_post',code_post,'code_post inconnu' from ban_temp b left join poste_cp p on (p.cp=b.code_post) where p.cp is null and b.code_post !=''" >> erreurs.csv
 
 echo "\n-- code_post ne figurant pas dans la base officielle\n"
-sql2csv --db "$DB" -H --query "select code_insee, 'code_post', code_post, 'code_post absent de la base officielle: '||cp from ban_temp b join (select insee, string_agg(cp,' ') as cp from poste_cp group by 1) as c on (code_insee=insee and not cp ~ code_post)" >> erreurs.csv
+sql2csv --db "$DB" -H --query "select code_insee, id, 'code_post', code_post, 'code_post absent de la base officielle: '||cp from ban_temp b join (select insee, string_agg(cp,' ') as cp from poste_cp group by 1) as c on (code_insee=insee and not cp ~ code_post)" >> erreurs.csv
 
