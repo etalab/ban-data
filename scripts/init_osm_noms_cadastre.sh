aabec8daa1a1ab78b4e6a0d8970f02bd9cdd3422
Christian Quest <cquest@openstreetmap.fr>
2015-05-20 15:06:55 +0200

    correction import

--- scripts/init_osm_noms_cadastre.sh
+++ scripts/init_osm_noms_cadastre.sh
@@ -1,8 +1,9 @@
 # import des noms de voie extraits des plan cadastraux par les scripts BANO
 
-zcat ../data/osm/noms_cadastre_win1252.csv.gz | iconv -f win1252 -t utf8 > noms_cadastre.csv
+cd ../data/osm/
+zcat noms_cadastre_win1252.csv.gz | iconv -f win1252 -t utf8 > noms_cadastre.csv
 
 psql -c "drop table if exists dgfip_noms_cadastre; create table dgfip_noms_cadastre (insee text, nom_cadastre text, fantoir text);"
-psql -c "\copy dgfip_noms_cadastre from '../data/osm/noms_cadastre.csv' with (format csv, header true);"
+psql -c "\copy dgfip_noms_cadastre from 'noms_cadastre.csv' with (format csv, header true);"
 psql -c "create index dgfip_noms_cadastre_fantoir on dgfip_noms_cadastre using spgist (fantoir);"
 
