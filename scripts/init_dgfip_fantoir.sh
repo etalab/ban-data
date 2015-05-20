8a9b8e7a0db266161c4b07eca1a13c6b174c2802
Christian Quest <cquest@openstreetmap.fr>
2015-05-20 14:06:06 +0200

    ajout wget fichier FANTOIR0415

--- scripts/init_dgfip_fantoir.sh
+++ scripts/init_dgfip_fantoir.sh
@@ -1,9 +1,9 @@
 # récupération et décompression du fichier FANTOIR
-# wget http://www2.impots.gouv.fr/documentation/fantoir/fichier_national_fantoir.zip
 
 FANTOIR=FANTOIR0415
 
 cd ../data/dgfip
+wget https://www.data.gouv.fr/s/resources/fichier-fantoir-des-voies-et-lieux-dits/20150512-104128/FANTOIR0415.zip
 unzip $FANTOIR.zip
 
 # import dans SQL en format fixe (delimiter et quote spéciaux pour ignorer)
