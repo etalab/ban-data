25a15031ddfeb16ec0985308dde19dcbad00541a
Christian Quest <cquest@openstreetmap.fr>
2015-05-27 11:04:12 +0200

    truncate du fichier au lieu de rm pour conserver hardlink

--- scripts/out_pg2json_all.sh
+++ scripts/out_pg2json_all.sh
@@ -2,7 +2,7 @@
 
 echo "`date +%H:%M:%S` Export JSON global"
 
-rm -f ../out/ban-odbl.json
+cat '' > ../out/ban-odbl.json
 
 # conversion/conformation/harmonisation en multi-thread des fichiers BAN en json pour addok
 parallel -j 1 sh out_pg2json.sh $1 >> ../out/ban-odbl.json {} ::: 01 02 03 04 05 06 07 08 09 `seq 10 19` 2A 2B `seq 21 95` `seq 971 974` 976
