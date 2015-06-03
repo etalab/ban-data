91534435d1670a004e8afc9b06a2313c41db1c0e
Christian Quest <cquest@openstreetmap.fr>
2015-06-03 20:00:28 +0200

    export dans ../out/odbl

--- scripts/out_odbl.sh
+++ scripts/out_odbl.sh
@@ -1,11 +1,12 @@
 # export BAN odbl postgres > shapefile, csv, json
 
 OUTDIR=../out/odbl
+DBPATH="dbname=cquest"
 
 mkdir $OUTDIR
 
 export SHAPE_ENCODING='UTF-8'
-ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 -s_srs "EPSG:4326" -t_srs "EPSG:4326" -overwrite $OUTDIR/BAN_odbl_$1.shp PG:"dbname=cquest" -sql "SELECT id, nom_voie, id_fantoir, numero, rep, code_insee, code_post, alias, nom_ld, x, y, nom_commune as commune, id_voie as fant_voie, id_ld as fant_ld, st_point(round(lat::numeric,6),round(lon::numeric,6)) from ban_$1 ORDER BY code_insee, fant_voie, fant_ld, nom_voie, nom_ld"
+ogr2ogr -f "ESRI Shapefile" -lco ENCODING=UTF-8 -s_srs "EPSG:4326" -t_srs "EPSG:4326" -overwrite $OUTDIR/BAN_odbl_$1.shp PG:"$DBPATH" -sql "SELECT id, nom_voie, id_fantoir, numero, rep, code_insee, code_post, alias, nom_ld, x, y, nom_commune as commune, id_voie as fant_voie, id_ld as fant_ld, st_point(round(lat::numeric,6),round(lon::numeric,6)) from ban_$1 ORDER BY code_insee, fant_voie, fant_ld, nom_voie, nom_ld"
 
 cd $OUTDIR
 zip -q -9 BAN_odbl_$1-shp.zip BAN_odbl_$1.* *.txt
