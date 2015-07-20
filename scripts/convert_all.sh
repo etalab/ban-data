729f9e6516b44682815b6f604a3ebcff7b5cec9f
root <root@ban.data.gouv.fr>
2015-07-20 15:12:53 +0200

    export complet, avec ODbL

--- scripts/convert_all.sh
+++ scripts/convert_all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# make sure we run this script with bash
+if [ ! "$BASH_VERSION" ] ; then
+    exec /bin/bash "$0" "$@"
+fi
+
 echo "`date +%H:%M:%S` début de traitement"
 
 # conversion/conformation/harmonisation en multi-thread des fichiers BAN en json pour addok
@@ -7,7 +12,11 @@ parallel -j 8 sh convert_ban2json.sh {} ::: 01 02 03 04 05 06 07 08 09 `seq 10 1
 
 echo "`date +%H:%M:%S` Fusion et compression des fichiers"
 # fusion en un fichier unique
-: > ../out/ban-odbl.json
-for dep in {01..19} 2A 2B {21..95} {971..974} ; do echo $dep; grep -v ^$ ../out/ban-odbl-$dep.json >>  ../out/ban-odbl.json ; gzip -9 ban-odbl-$dep.json ; done
+truncate -s 0 ../out/ban-odbl.json
+for dep in {01..19} 2A 2B {21..95} {971..976} ; do
+	grep -v '^$' "../out/ban-odbl-$dep.json" >> ../out/ban-odbl.json
+	cat ../out/ban-odbl-$dep.json | bzip2 -9 -c > ../out/odbl/BAN_odbl-$dep.json.bz2
+	sh out_odbl.sh $dep
+done
 
 echo "`date +%H:%M:%S` fin de traitement"
