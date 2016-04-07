0fe565418fbdc5490441b9d22c5a410979b11fd9
cquest <christian.quest@data.gouv.fr>
2016-04-07 16:11:48 +0200

    import communes OSM 2015

--- scripts/init_osm_communes.sh
+++ scripts/init_osm_communes.sh
@@ -9,3 +9,11 @@ create index osm_communes_index on osm_communes using gist(wkb_geometry);
 create index osm_communes_insee on osm_communes (insee);
 "
 
+wget -nc http://osm13.openstreetmap.fr/~cquest/openfla/export/communes-20150101-5m-shp.zip
+unzip -o communes-20150101-5m-shp.zip
+ogr2ogr -t_srs EPSG:4326 -f PostgreSQL PG: communes-plus-20150101-5m-shp/communes-plus-20150101-5m.shp -overwrite -nlt GEOMETRY -nln osm_communes_2015 -skipfailures
+psql -c "
+create index osm_communes_2015_index on osm_communes_2015 using gist(wkb_geometry);
+create index osm_communes_2015_insee on osm_communes_2015 (insee);
+"
+
