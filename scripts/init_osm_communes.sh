5671976dc2ba9bad9a5f9bf58f3478183182d5fd
cquest <cquest@openstreetmap.fr>
2017-01-30 12:43:12 +0100

    import région 2016 (nouveaux noms)

--- scripts/init_osm_communes.sh
+++ scripts/init_osm_communes.sh
@@ -17,3 +17,13 @@ create index osm_communes_2015_index on osm_communes_2015 using gist(wkb_geometr
 create index osm_communes_2015_insee on osm_communes_2015 (insee);
 "
 
+
+# liste des régions 2016 (noms finaux)
+wget -nc http://osm13.openstreetmap.fr/~cquest/openfla/export/regions-20170102-shp.zip
+unzip regions-20170102-shp.zip
+ogr2ogr -t_srs EPSG:4326 -f PostgreSQL PG: regions-20170102.shp -overwrite -nlt GEOMETRY -nln osm_regions_2016 -skipfailures
+rm regions-20170102*
+psql -c "
+create index osm_regions_2016_index on osm_regions_2016 using gist(wkb_geometry);
+create index osm_regions_2016_insee on osm_regions_2016 (insee);
+"
