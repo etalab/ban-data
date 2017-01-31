1dc0ac1045d14d4ee4c95ec1e96f053ab2dd160d
cquest <cquest@openstreetmap.fr>
2017-01-31 08:38:41 +0100

    codes postaux 2016

--- scripts/init_poste_cp.sh
+++ scripts/init_poste_cp.sh
@@ -1,16 +1,12 @@
 # import de la liste des codes postaux
-# c'est une version corrigée qui est utilisée, le fichier officiel sur data.gouv.fr comportant des erreurs
 
 mkdir ../data/poste/
 cd ../data/poste/
-wget -nc https://www.data.gouv.fr/s/resources/base-officielle-des-codes-postaux/community/20150308-152148/code_postaux_v201410_corr.csv
 
-# import dans postgres
-psql -c "drop table if exists poste_cp; create table poste_cp (insee text, commune text, cp text, libelle text);"
-psql -c "\copy poste_cp from code_postaux_v201410_corr.csv with (format csv, header true, encoding 'iso8859-1', delimiter ';');"
+wget -O codes_postaux.csv "https://datanova.legroupe.laposte.fr/explore/dataset/laposte_hexasmal/download/?format=csv"
+psql -c "drop table if exists poste_cp; create table poste_cp (insee text, commune text, cp text, libelle text ligne5 text, coords text);"
+psql -c "\copy poste_cp from codes_postaux.csv with (format csv, header true, delimiter ';');"
 psql -c "
 create index poste_cp_insee on poste_cp using spgist (insee);
 create index poste_cp_cp on poste_cp using spgist (cp);
 "
-
-
