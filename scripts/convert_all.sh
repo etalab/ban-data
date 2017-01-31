bf6ebdfbc74a2150975d36d3b7dec9000e537995
cquest <cquest@openstreetmap.fr>
2017-01-31 08:36:25 +0100

    simplification traitement global

--- scripts/convert_all.sh
+++ scripts/convert_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# liste des départements à traiter
+DEPTS=$(echo `seq -w 1 19` 2A 2B `seq 21 95` `seq 971 976`)
+
 # make sure we run this script with bash
 if [ ! "$BASH_VERSION" ] ; then
     exec /bin/bash "$0" "$@"
@@ -8,14 +11,16 @@ fi
 echo "`date +%H:%M:%S` début de traitement"
 
 # conversion/conformation/harmonisation en multi-thread des fichiers BAN en json pour addok
-parallel -j 8 sh convert_ban2json.sh {} ::: 01 02 03 04 05 06 07 08 09 `seq 10 19` 2A 2B `seq 21 95` `seq 971 976`
+parallel -j 6 sh convert_ban2json.sh {} ::: $DEPTS
+
+# export des fichiers CSV, SHP
+parallel -j 6 sh out_odbl.sh {} ::: $DEPTS
 
 echo "`date +%H:%M:%S` Fusion des fichiers json"
 # fusion en un fichier unique
 truncate -s 0 ../out/ban-odbl.json
-for dep in {01..19} 2A 2B {21..95} {971..976} ; do
-	sh out_odbl.sh $dep
-	grep -v '^$' "../out/ban-odbl-$dep.json" >> ../out/ban-odbl.json
+for dep in $DEPTS ; do
+  grep -v '^$' "../out/ban-odbl-$dep.json" >> ../out/ban-odbl.json
 done
 
 echo "`date +%H:%M:%S` Compression du fichier fusionné"
