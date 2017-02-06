9282ee8c065a589cec4a35a18078bc832b5045e3
cquest <cquest@openstreetmap.fr>
2017-02-06 17:17:07 +0100

    abreviations via table distincte

--- scripts/abrev_lib.sh
+++ scripts/abrev_lib.sh
@@ -15,8 +15,6 @@ insert into libelles (select upper(unaccent(nom_voie)) as long, null as court fr
 -- libellés BAN (nom_ld)
 insert into libelles (select nom_ld as long, null as court from ban_temp left join libelles on (long=nom_ld) where long is null group by 1,2);
 
--- libellés BAN (alias)
-insert into libelles (select alias as long, null as court from ban_full left join libelles on (long=alias) where long is null group by 1,2);
 
 --
 update libelles set court=regexp_replace(regexp_replace(long,'[^A-Z 0-9]',' ','g'),' +',' ','g') where court is null;
