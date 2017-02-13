1bca879f16f9a109700ddf9283cb7ddbc9f8c909
Christian Quest <cquest@openstreetmap.fr>
2017-02-13 15:01:21 +0100

    really closes #71

--- scripts/out_pg2json.sh
+++ scripts/out_pg2json.sh
@@ -40,22 +40,21 @@ select format('{\"id\":\"%s_%s\",\"type\":\"%s\",\"name\":\"%s\",\"postcode\":\"
   housenumbers)
 from
 (select code_insee,
-
 (case when id_ld is not null AND coalesce(id_voie,'')!=coalesce(id_ld,'') then coalesce(id_voie,'')||id_ld
 when coalesce(id_voie,'')!='' and nom_ld='' then coalesce(id_voie,'')
 when id_ld is not null then id_ld
 when coalesce(id_voie,'')!='' and nom_ld!='' then coalesce(id_voie,'')
 else
 'XXXX' end) || '_' || left(md5(format('n=%s,l=%s,a=%s,p=%s',unaccent(nom_voie),nom_ld,alias,code_post)),6) as fantoir,
-replace(case when (coalesce(nom_voie,'') !='' and coalesce(nom_ld,'') !='' and replace(upper(unaccent(coalesce(nom_voie,''))),'-',' ')!=replace(upper(unaccent(coalesce(nom_ld,''))),'-',' ')) then (coalesce(nom_voie,'')||', '||coalesce(nom_ld,'')) when (coalesce(nom_voie,'')='') then nom_ld else nom_voie end,'\"','') as  nom_voie,
+replace(case when (coalesce(max(nom_voie),'') !='' and coalesce(max(nom_ld),'') !='' and replace(upper(unaccent(coalesce(max(nom_voie),''))),'-',' ')!=replace(upper(unaccent(coalesce(max(nom_ld),''))),'-',' ')) then (coalesce(max(nom_voie),'')||', '||coalesce(max(nom_ld),'')) when (coalesce(max(nom_voie),'')='') then max(nom_ld) else max(nom_voie) end,'\"','') as  nom_voie,
 code_post,
 round(avg(lat::numeric),6) as lat,
 round(avg(lon::numeric),6) as lon,
-regexp_replace(nom_commune,' [0-9].*','') as nom_commune,
-dr.nom_dep,
-case when dr.nom_reg=dr.nom_reg2016 then dr.nom_reg2016 else format('%s (%s)', dr.nom_reg2016, dr.nom_reg) end as nom_reg,
-case when coalesce(id_voie,id_ld,id_fantoir) > '9999' then 'locality' else 'street' end as type,
-round(log((CASE WHEN (code_post LIKE '75%' OR g.statut LIKE 'Capital%') THEN 6 WHEN (code_post LIKE '690%' OR code_post LIKE '130%' OR g.statut = 'Préfecture de régi') THEN 5 WHEN g.statut='Préfecture' THEN 4 WHEN g.statut LIKE 'Sous-pr%' THEN 3 WHEN g.statut='Chef-lieu canton' THEN 2 ELSE 1 END)+log(g.population+1)/3)::numeric*log(1+log(count(b.*)+1)+log(CASE WHEN nom_voie like 'Boulevard%' THEN 4 WHEN nom_voie LIKE 'Place%' THEN 4 WHEN nom_voie LIKE 'Espl%' THEN 4 WHEN nom_voie LIKE 'Av%' THEN 3 WHEN nom_voie LIKE 'Rue %' THEN 2 ELSE 1 END))::numeric,4)::text as importance,
+regexp_replace(max(nom_commune),' [0-9].*','') as nom_commune,
+max(dr.nom_dep) as nom_dep,
+max(case when dr.nom_reg=dr.nom_reg2016 then dr.nom_reg2016 else format('%s (%s)', dr.nom_reg2016, dr.nom_reg) end) as nom_reg,
+max(case when coalesce(id_voie,id_ld,id_fantoir) > '9999' then 'locality' else 'street' end) as type,
+round(log((CASE WHEN (code_post LIKE '75%' OR max(g.statut) LIKE 'Capital%') THEN 6 WHEN (code_post LIKE '690%' OR code_post LIKE '130%' OR max(g.statut) = 'Préfecture de régi') THEN 5 WHEN max(g.statut)='Préfecture' THEN 4 WHEN max(g.statut) LIKE 'Sous-pr%' THEN 3 WHEN max(g.statut)='Chef-lieu canton' THEN 2 ELSE 1 END)+log(max(g.population)+1)/3)::numeric*log(1+log(count(b.*)+1)+log(CASE WHEN max(nom_voie) like 'Boulevard%' THEN 4 WHEN max(nom_voie) LIKE 'Place%' THEN 4 WHEN max(nom_voie) LIKE 'Espl%' THEN 4 WHEN max(nom_voie) LIKE 'Av%' THEN 3 WHEN max(nom_voie) LIKE 'Rue %' THEN 2 ELSE 1 END))::numeric,4)::text as importance,
 string_agg(format('\"%s\":{\"lat\":%s,\"lon\":%s,\"id\":\"%s\"}',trim(numero||' '||rep),round(lon::numeric,6)::text,round(lat::numeric,6)::text,id),',' order by numero||rep,id) as housenumbers,
 max(case when cn.insee is not null and upper(unaccent(nom_commune)) != upper(unaccent(g2.nom)) then format('(%s)',g2.nom) else '' end) as ancienne_commune
 from ban_$1 b
@@ -66,6 +65,6 @@ join dep_reg_2016 dr on (dr.dep=d.dep)
 left join insee_communes_nouvelles cn on (cn.insee=b.code_insee)
 left join osm_communes_2015 g2 on (st_contains(g2.wkb_geometry, b.geom))
 where nom_voie||nom_ld!=''
-group by 1,2,3,4,7,8,9,10,g.statut,g.population,nom_voie, nom_commune
+group by 1,2,4
 order by 1,2,3) as d;
 "
