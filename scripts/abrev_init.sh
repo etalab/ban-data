e77e04fe2faa84da20e4fbf1ecc5aa79064ba3ab
cquest <cquest@openstreetmap.fr>
2017-02-15 12:29:18 +0100

    simplification init abbrev libelles

--- scripts/abrev_init.sh
+++ scripts/abrev_init.sh
@@ -8,14 +8,6 @@ create index libelles_court on libelles (court);
 create index libelles_court_trgm on libelles using gist (court gist_trgm_ops);
 
 -- libelles FANTOIR
-insert into libelles (select lib_court as long, null as court from dgfip_fantoir left join libelles on (long=lib_court) where long is null group by 1,2);
+insert into libelles (select lib_court as long, regexp_replace(regexp_replace(lib_court,'[^A-Z 0-9]',' ','g'),' +',' ','g') as court from dgfip_fantoir group by 1,2);
 
--- libellés BAN (nom_voie)
-insert into libelles (select upper(unaccent(nom_voie)) as long, null as court from ban_temp left join libelles on (long= upper(unaccent(nom_voie))) where long is null group by 1,2);
-
--- libellés BAN (nom_ld)
-insert into libelles (select nom_ld as long, null as court from ban_temp left join libelles on (long=nom_ld) where long is null group by 1,2);
-
--- nettoyage libellés courts pour ne garder que lettres et chiffres
-update libelles set court=regexp_replace(regexp_replace(long,'[^A-Z 0-9]',' ','g'),' +',' ','g') where court is null;
 "
