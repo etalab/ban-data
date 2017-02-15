410bf9c8c5bd0c11cf8f00a38ad7011de9bb4d50
cquest <cquest@openstreetmap.fr>
2017-02-15 11:58:40 +0100

    pas d'abrev dernier mot + ajout article 'EN'

--- scripts/abrev_lib.sh
+++ scripts/abrev_lib.sh
@@ -1,7 +1,8 @@
 # création et mise à jour de la table des libellés
 
 psql -c "
-create table if not exists libelle (long text, court text);
+drop table if exists libelle;
+create table libelle (long text, court text);
 create index libelles_long on libelles (long);
 create index libelles_court on libelles (court);
 create index libelles_court_trgm on libelles using gist (court gist_trgm_ops);
@@ -15,8 +16,7 @@ insert into libelles (select upper(unaccent(nom_voie)) as long, null as court fr
 -- libellés BAN (nom_ld)
 insert into libelles (select nom_ld as long, null as court from ban_temp left join libelles on (long=nom_ld) where long is null group by 1,2);
 
-
---
+-- nettoyage libellés courts pour ne garder que lettres et chiffres
 update libelles set court=regexp_replace(regexp_replace(long,'[^A-Z 0-9]',' ','g'),' +',' ','g') where court is null;
 "
 
@@ -28,12 +28,12 @@ psql -c "\copy abbrev from ../data/abbreviations.csv WITH (FORMAT CSV, HEADER TR
 psql -c "UPDATE libelles SET court=regexp_replace(court,' ([A-Z0-9]+) \1 ',' \1 ','g') WHERE court ~ ' ([A-Z0-9]+) \1 ';"
 
 # génération et exécution de requêtes UPDATE
-psql -Atc "select format(E'UPDATE libelles SET court=regexp_replace(court,\'%s\',\'%s\') WHERE court ~ \'%s \';', '(^| )'||txt_long||'( |$)', '\1'||txt_court||' ',txt_long) from abbrev WHERE txt_long!=txt_court order by length(txt_long) desc;" | psql
+psql -Atc "select format(E'UPDATE libelles SET court=regexp_replace(court,\'%s\',\'%s\') WHERE court ~ \'%s \';', '(^| )'||txt_long||' ', '\1'||txt_court||' ',txt_long) from abbrev WHERE txt_long!=txt_court order by length(txt_long) desc;" | psql
 
 # suppression des articles
-psql -c "UPDATE libelles SET court=regexp_replace(court,' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE) ',' ','g') WHERE court ~ ' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE) ';"
-psql -c "UPDATE libelles SET court=regexp_replace(court,' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE) ',' ','g') WHERE court ~ ' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE) ';"
-psql -c "UPDATE libelles SET court=regexp_replace(court,' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE) ',' ','g') WHERE court ~ ' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE) ';"
+psql -c "UPDATE libelles SET court=regexp_replace(court,' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE|EN) ',' ','g') WHERE court ~ ' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE|EN) ';"
+psql -c "UPDATE libelles SET court=regexp_replace(court,' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE|EN) ',' ','g') WHERE court ~ ' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE|EN) ';"
+psql -c "UPDATE libelles SET court=regexp_replace(court,' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE|EN) ',' ','g') WHERE court ~ ' (DE|DU|DE LA|DE L|D|L|DES|AU|AUX|A|ET|DIT|DITE|EN) ';"
 
 # simplification des XXième, ème, er...
 psql -c "UPDATE libelles SET court=regexp_replace(court,' ([0-9]+)(IE|E)[A-Z]+ ',' \1E ','g') WHERE court ~ ' ([0-9]+)(IE|E)[A-Z]+ ';"
