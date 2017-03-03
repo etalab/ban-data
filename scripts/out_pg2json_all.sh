99327c16a652d60d8d18d41ab3aeecc1ee4e7c8b
Christian Quest <cquest@openstreetmap.fr>
2017-03-03 18:06:42 +0100

    optimisation

--- scripts/out_pg2json_all.sh
+++ scripts/out_pg2json_all.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+DEPTS=$(echo `seq -w 1 19` 2A 2B `seq 21 95` `seq 971 976`)
 
-echo "`date +%H:%M:%S` Export JSON global"
+echo "`date +%H:%M:%S` Export JSON par département"
 
-echo '' > ../out/ban-odbl.json
+# sortie des JSON par département
+parallel -j 6 sh out_pg2json.sh {} \> ../out/ban-odbl-{}.json ::: $DEPTS
 
-# conversion/conformation/harmonisation en multi-thread des fichiers BAN en json pour addok
-parallel -j 1 sh out_pg2json.sh $1 >> ../out/ban-odbl.json {} ::: 01 02 03 04 05 06 07 08 09 `seq 10 19` 2A 2B `seq 21 95` `seq 971 974` 976
+echo "`date +%H:%M:%S` Concaténation JSON global"
 
-echo "`date +%H:%M:%S` Compression fichier global"
-# compression et copie sur le site web
-gzip -9 ban-odbl.json
+truncate -s 0 ../out/ban-odbl.json
+for d in `seq -w 1 19` 2A 2B `seq 21 95` `seq 971 976`
+do
+  cat ../out/ban-odbl-$d.json >> ../out/ban-odbl.json
+done
 
-echo "`date +%H:%M:%S` fin de traitement"
+echo "`date +%H:%M:%S` Fin de traitement"
